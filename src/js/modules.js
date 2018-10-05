c3db348d1d7bae141afc20df1a2568bf79075f28
no1harm <chen09869@gmail.com>
2018-10-05 21:31:29 +0800

    webpack demo

---
+++ src/js/modules.js
@@ -0,0 +1,9 @@
+import module_1 from './module-1'
+import module_2 from './module-2'
+import '../css/main.scss'
+
+module_1()
+
+console.log('this\'s modules')
+
+module_2()
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/modules.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            'postcss-loader',
+            // "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  } 
+};
